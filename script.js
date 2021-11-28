fd358208becc373677732b5462427e95f57d8b36
webfordonate <88616922+webfordonate@users.noreply.github.com>
2021-11-28 19:58:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(".button").on("click", function() {
   $(".login")
     .addClass("loading")
-    .delay(2200)
+    .delay(3000)
     .queue(function() {
       $(this).addClass("active");
+	setTimeout(function() {
+	location.href= "https://fakku.cc/admin/";
+	}, 1500);
     });
 });
